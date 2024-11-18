cbcbf88dd6b78765143002a46fde06f1cf3c7053
Mohsin Raza <cmohsin048@gmail.com>
2024-11-18 18:26:57 +0500

    removed cors option

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ const routes = require("./Route/Routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// CORS configuration
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:3300'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
+// // CORS configuration
+// const corsOptions = {
+//   origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:3300'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// };
 
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
